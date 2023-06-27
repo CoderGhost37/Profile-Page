3db992abd48b9252dac035ec18c2946064cf51c8
CoderGhost37 <coderghost37@gmail.com>
2023-06-27 16:09:51 +0530

    feat: resume page

---
+++ src/components/EditProfile/Card.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { AiOutlineDelete } from 'react-icons/ai';
+
+import image from '@/constant/image';
+
+interface CardProps {
+  icon?: string;
+  img?: string;
+  name: string;
+  location: string;
+  duration: string;
+  company?: string;
+  degree?: string;
+  desc?: string;
+  responsibilities?: string[];
+  handleDelete: () => void;
+}
+
+const Card = ({
+  icon,
+  img,
+  name,
+  location,
+  duration,
+  company,
+  degree,
+  desc,
+  responsibilities,
+  handleDelete,
+}: CardProps) => {
+  return (
+    <div className='mt-2 flex w-full'>
+      <div className='mr-4'>
+        {img && (
+          // eslint-disable-next-line @next/next/no-img-element
+          <img src={img} alt={name} className='mt-2 h-7 w-7 rounded-full' />
+        )}
+        {icon && image(icon)}
+      </div>
+      <div className='flex w-full flex-col'>
+        <div className='flex w-full items-center justify-between'>
+          <h4>{name}</h4>
+          <AiOutlineDelete
+            onClick={handleDelete}
+            className='ml-2 cursor-pointer rounded-full p-0.5 text-2xl text-red-500 hover:bg-gray-100'
+          />
+        </div>
+        <div className='flex w-full flex-col justify-between md:flex-row md:items-center'>
+          <div className='flex flex-wrap text-sm'>
+            <p className='mr-1'>{location}</p>
+            {company && <p>• {company}</p>}
+            {degree && <p>• {degree}</p>}
+          </div>
+          <h5 className='text-sm font-semibold'>{duration}</h5>
+        </div>
+        {desc && <p className='mt-2 text-base text-gray-500'>{desc}</p>}
+        {responsibilities && (
+          <div className=''>
+            <h4 className='mt-2 text-base'>Job responsibilities:</h4>
+            <ul className='list-inside list-disc text-base text-gray-500'>
+              {responsibilities?.map((item, index) => (
+                <li key={index}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/components/EditProfile/Resume.tsx
+++ src/components/EditProfile/Resume.tsx
@@ -1,7 +1,172 @@
 import React from 'react';
+import { AiOutlinePlus } from 'react-icons/ai';
+
+import ButtonGroup from '@/components/ButtonGroup';
+import Card from '@/components/EditProfile/Card';
+import MultiFieldInput from '@/components/MultiFieldInput';
+import MultiTagInput from '@/components/MultiTagInput';
+
+import { Education, resumeData, WorkExperience } from '@/constant/data';
 
 const Resume = () => {
-  return <div>Resume</div>;
+  const [form, setForm] = React.useState({
+    about: resumeData.about,
+    education: resumeData.education,
+    workExp: resumeData.workExperience,
+    interests: resumeData.interests,
+  });
+  const [tags, setTags] = React.useState<string[]>(resumeData.interests);
+  const [addEducation, setAddEducation] = React.useState(false);
+  const [addWorkExperience, setAddWorkExperience] = React.useState(false);
+
+  const onDeleteEducation = (index: number) => {
+    const newEducation = [...form.education];
+    newEducation.splice(index, 1);
+    setForm({
+      ...form,
+      education: newEducation,
+    });
+  };
+
+  const onDeleteWorkExp = (index: number) => {
+    const newWorkExp = [...form.workExp];
+    newWorkExp.splice(index, 1);
+    setForm({
+      ...form,
+      workExp: newWorkExp,
+    });
+  };
+
+  const onSaveEducation = (fields: Education) => {
+    if (!fields)
+      setForm({
+        ...form,
+        education: [fields, ...form.education],
+      });
+    setAddEducation(false);
+  };
+
+  const onSaveWorkExp = (fields: WorkExperience) => {
+    if (!fields)
+      setForm({
+        ...form,
+        workExp: [fields, ...form.workExp],
+      });
+    setAddWorkExperience(false);
+  };
+
+  const handleCancel = () => {
+    setForm({
+      about: resumeData.about,
+      education: resumeData.education,
+      workExp: resumeData.workExperience,
+      interests: resumeData.interests,
+    });
+  };
+
+  const handleSave = () => {
+    setForm({
+      ...form,
+      interests: tags,
+    });
+  };
+
+  return (
+    <div className='w-full px-6'>
+      <div className='flex flex-col items-center justify-between space-y-8'>
+        <div className='flex w-full flex-col'>
+          <label className='font-semibold'>About</label>
+          <textarea
+            name='about'
+            id='about'
+            rows={7}
+            className='mt-1 w-full rounded-lg border border-gray-300 px-4 py-2 focus:border-blue-300 focus:outline-none md:text-lg'
+            placeholder='Enter about yourself'
+            value={form.about}
+            onChange={() =>
+              setForm({
+                ...form,
+                about: (document.getElementById('about') as HTMLTextAreaElement)
+                  .value,
+              })
+            }
+          />
+        </div>
+        <div className='flex w-full flex-col gap-4'>
+          <div className='flex w-full items-center justify-between'>
+            <label className='text-lg font-semibold'>Work Experience</label>
+            {!addWorkExperience && (
+              <button
+                onClick={() => setAddWorkExperience(true)}
+                className='flex items-center rounded-lg bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600'
+              >
+                <AiOutlinePlus /> Add
+              </button>
+            )}
+          </div>
+          {addWorkExperience && (
+            <MultiFieldInput
+              type='Work Experience'
+              handleClose={() => setAddWorkExperience(false)}
+              handleSaveWorkExp={(fields: WorkExperience) =>
+                onSaveWorkExp(fields)
+              }
+              handleSaveEducation={(fields: Education) =>
+                onSaveEducation(fields)
+              }
+            />
+          )}
+          {form.workExp.map((item, index) => (
+            <Card
+              key={index}
+              {...item}
+              handleDelete={() => onDeleteWorkExp(index)}
+            />
+          ))}
+        </div>
+        <div className='flex w-full flex-col gap-4'>
+          <div className='flex w-full items-center justify-between'>
+            <label className='text-lg font-semibold'>Education</label>
+            {!addEducation && (
+              <button
+                onClick={() => setAddEducation(true)}
+                className='flex items-center rounded-lg bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-600'
+              >
+                <AiOutlinePlus /> Add
+              </button>
+            )}
+          </div>
+          {addEducation && (
+            <MultiFieldInput
+              type='Education'
+              handleClose={() => setAddEducation(false)}
+              handleSaveWorkExp={(fields: WorkExperience) =>
+                onSaveWorkExp(fields)
+              }
+              handleSaveEducation={(fields: Education) =>
+                onSaveEducation(fields)
+              }
+            />
+          )}
+          {form.education.map((item, index) => (
+            <Card
+              key={index}
+              {...item}
+              handleDelete={() => onDeleteEducation(index)}
+            />
+          ))}
+        </div>
+        <MultiTagInput label='Interests' tags={tags} setTags={setTags} />
+        <ButtonGroup
+          classStyles='justify-end w-full mt-4'
+          button1_text='Cancel'
+          button2_text='Save changes'
+          button1_onClick={handleCancel}
+          button2_onClick={handleSave}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Resume;
